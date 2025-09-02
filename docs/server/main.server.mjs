9febfb32296f7749f8df13f45647fd47034e50bc
theobizet <theobizet@outlook.fr>
2025-09-02 11:28:14 +0200

    correction de bug V2

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-F7SJNSXI.mjs";import"./chunk-Z7LZLLY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QJMLBXSO.mjs";import"./chunk-Z7LZLLY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
