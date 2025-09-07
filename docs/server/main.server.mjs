588a256b38228f8ef25b4ffa22b1df8d50d4b940
theobizet <theobizet@outlook.fr>
2025-09-07 17:07:23 +0200

    Améliorations globales

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NOT6ZK7Z.mjs";import"./chunk-OWPLEPJM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-4H46P3PM.mjs";import"./chunk-OWPLEPJM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
