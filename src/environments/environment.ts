9f512182125856cf1d2bd00cfc07c9f028c20fc5
theobizet <theobizet@outlook.fr>
2025-09-21 20:29:25 +0200

    access token update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   dialogflowProjectId: 'chatbot-portfolio-iqcd', // Remplace par ton ID de projet
-  dialogflowAccessToken: '1081549911464', // Généré depuis Google Cloud
+  dialogflowAccessToken: 'AIzaSyCn-P0EvnM53QVKGNOJOA_ApBSp5741Uao', // Généré depuis Google Cloud
 };
